c0beb5810906ab11e0763dcc810978c7ae3e1118
Oliu <lifen.zhuo@gmail.com>
2017-05-22 11:35:00 +0800

    init

---
+++ weaver-web/src/main/java/com/formicine/weaver/WeaverWebApplication.java
@@ -0,0 +1,12 @@
+package com.formicine.weaver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeaverWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeaverWebApplication.class, args);
+	}
+}
---
+++ weaver-web/src/main/java/com/formicine/weaver/controller/IndexController.java
@@ -0,0 +1,17 @@
+package com.formicine.weaver.controller;
+
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Created by zli on 2017/5/19.
+ */
+@Controller
+public class IndexController {
+    @RequestMapping(value = "", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    public String index() {
+        return "index";
+    }
+}
